94ffd46e9d51c49f27255853c0173ccce2dc4813
AleksMaifet <aleksmaifet@gmail.com>
2022-12-19 17:54:12 +0300

    updated form logic

--- src/components/Forms/Form/index.tsx
+++ src/components/Forms/Form/index.tsx
@@ -56,7 +56,6 @@ const Form: React.FC<IForm> = ({
     <FormWrapper onSubmit={handleSubmit(onSubmit)}>
       <FormInformLine>
         <Line>
-          2
           <PrimaryInput
             placeholder={emailPlaceholder}
             styleOptions={style.firstStage}
